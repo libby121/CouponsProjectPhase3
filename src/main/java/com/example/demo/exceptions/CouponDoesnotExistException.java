95339517f96546a2bd060dcda798d47427684e68
Sourav <36604593+souravCoder1@users.noreply.github.com>
2025-03-14 18:24:55 +0530

    Update CouponDoesnotExistException.java

--- src/main/java/com/example/demo/exceptions/CouponDoesnotExistException.java
+++ src/main/java/com/example/demo/exceptions/CouponDoesnotExistException.java
@@ -8,6 +8,7 @@ public class CouponDoesnotExistException extends Exception {
 	private static final long serialVersionUID = 1L;
 	public CouponDoesnotExistException() {
 		super("oops..coupon does not exist");
+		
 	}
 
 }
